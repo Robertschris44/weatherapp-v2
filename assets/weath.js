111f78217c02b2e71789ddca9a2089b6476bea03
Chris Key <robertschris010@gmail.com>
2022-07-20 23:06:12 -0500

    creating elements in js

--- assets/weath.js
+++ assets/weath.js
@@ -4,3 +4,24 @@ var searchListEl = document.querySelector("#recents");
 var todayWeatherEl =document.querySelector("#currentStatus");
 var currentDateEl = document.querySelector("#currentDate");
 var containerEl = document.querySelector("#extendedWeather");
+
+
+
+var formSubmit =function(event) {
+    event.preventDefault();
+     
+    var searchCity = document.querySelector("input[name='inputCity']").value;
+
+    var listEl = document.createElement("li");
+
+
+
+
+
+
+
+
+
+
+
+}
