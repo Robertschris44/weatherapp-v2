e4a17f84f6d1459de4148c8b3d38659296c7d3b3
Chris Key <robertschris010@gmail.com>
2022-07-20 22:48:25 -0500

    created variables in js

---
+++ assets/weath.js
@@ -0,0 +1,6 @@
+var citySearchEl = document.querySelector("formLookup");
+var cityEl =document.querySelector("#inputCity");
+var searchListEl = document.querySelector("#recents");
+var todayWeatherEl =document.querySelector("#currentStatus");
+var currentDateEl = document.querySelector("#currentDate");
+var containerEl = document.querySelector("#extendedWeather");
