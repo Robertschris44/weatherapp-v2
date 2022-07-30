213d5cafabce72f2a6915bec57fdc6cce1e32e02
Chris Key <robertschris010@gmail.com>
2022-07-29 20:46:14 -0500

    starting different script for better results

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var uValue = $(".uvi");
+var searchbtn = $("search-btn");
+var apiKey = "d899707429dae12637678613a5874634"
+var searchInput = "";
+var currentTemp = $("#currenttemp");
+var currentWind = $("#currentwind");
+var currentHumidity = $("#currenthumidity");
+var currentUv = $("#currentUVindex");
+var currentDate = moment().format("M/D/YYYY");
+var cityName = "";
+var dailyDivs 
+
+
--- assets/weath.js
+++ assets/weath.js
@@ -4,41 +4,61 @@ var searchListEl = document.querySelector("#recents");
 var todayWeatherEl =document.querySelector("#currentStatus");
 var currentDateEl = document.querySelector("#currentDate");
 var containerEl = document.querySelector("#extendedWeather");
+var apiKey = "d899707429dae12637678613a5874634";
+var searchButton = document.querySelector("#citySubmit");
 
-
-
-var formSubmit =function(event) {
-    event.preventDefault();
+ searchButton.click (function() {
+   console.log("submitButton")
      
     var searchCity = document.querySelector("input[name='inputCity']").value;
 
     var listEl = document.createElement("li");
     listEl.className = "search-item";
+    listEl.appendChild(searchCity);
 
     var searchInfoEl = document.createElement("div");
      
     searchInfoEl.className = "search-info";
 
     searchInfoEl.innerHTML = "<h3 class=searchCity'>" + searchCity + "</h3>";
-    listEl.appendChild(searchCity);
+    
 
     searchCity.appendChild(listEl);
 
-    var cityInput = cityEl.value.trim();
-    if (cityInput) {
-        getForecast(cityInput);
-    }
+    
+        getlatandlon(cityInput);
+    
 
-    formSearch.reset();
-};
+});
 
-
-
-
-
-
-
-
-
-
+ function getlatandlon(searchInput){
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + searchInput + "&limit=1&appid=" + apiKey;
+    fetch(apiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+            console.log(data);
+              // Gets the lon and lat of the location
+              var locationLat = data[0].lat;
+              var locationLon = data[0].lon;
+              cityName = data[0].name;
+              // Convert from Int to Str
+              var latString = locationLat.toString();
+              var lonString = locationLon.toString();
+              // Call function to get values
+              getForecast(latString, lonString);
+            });
+        } else {
+            alert("Location Not Found");
+        }
+    });   
+ }
+var getForecast = function(cityInput) {
+    var apiUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data);
+            })
+        }
+    })
 }
