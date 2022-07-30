a307a595bae9d596c3fbca782c773699d558d742
Chris Key <robertschris010@gmail.com>
2022-07-29 22:12:20 -0500

    fetching api

--- assets/script.js
+++ assets/script.js
@@ -21,11 +21,37 @@ for (var i =0; i <storedCities.length; i ++) {
 
 searchButton.click(function(){
     console.log ("btnwasclicked")
+    //trimmed empty spaces in inputcity
     searchInput = $("#inputcity").val().trim();
     var previousSavedCities = JSON.parse(localStorage.getItem("storedCites")) || []
     previousSavedCities.push(searchInput)
+    //working on local storage
     localStorage.setItem("storedCities", JSON.stringify(previousSavedCities))
     getUserlocation(searchInput); 
 });
+
+function getSavedWeather(){
+    getUserlocation($(this).text())
+}
+
+function getUserlocation (searchInput) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?" + searchInput + "&units=imperial&appid=8e8a4571fd74feffb1e4f730b5eb774b"
+    fetch(apiUrl).then(function(response){
+        if (response.ok){
+            response.json().then(function(data){
+                console.log(data);
+                var loclat = data[0].lat;
+                var loclon = data[0].lon;
+                cityName = data [0].name;
+            })
+        }
+    })
+    
+}
+
+
+function getUserlocation (searchInput) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey;
+}
 
 
